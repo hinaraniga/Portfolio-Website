9349160baf3f587b45523534187aec2c0c40bcbc
hinaraniga <hina.raniga@hotmail.co.uk>
2022-10-04 15:13:16 +0100

    Fixes following cross-browser & viewport testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-console.log("Hello! Welcome to my UI Design Portfolio Site!");
-function menuToggle() {
+console.log('Hello! Welcome to my UI Design Portfolio Site!');
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
